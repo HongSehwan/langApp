2ab72acb09e09cc46893678d79716a16d89c11bd
Hong <gg9297@gmail.com>
2022-02-19 01:39:10 +0900

    Add data

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Animated } from "react-native";
-import styled from "styled-components";
+import { Animated, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
@@ -8,17 +8,21 @@ const Container = styled.View`
   align-items: center;
 `;
 
-const Box = styled(Animated.createAnimatedComponent(TouchableOpacity))`
+const Box = styled.TouchableOpacity`
   background-color: tomato;
   width: 200px;
   height: 200px;
 `;
 
-const AnimatedBox = createAnimatedComponent(Box);
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const Y = Animated.Value(0);
-  const moveUp = () => {};
+  const Y = new Animated.Value(0);
+  const moveUp = () => {
+    Animated.timing(Y, {
+      toValue: 200,
+    });
+  };
   return (
     <Container>
       <Box
